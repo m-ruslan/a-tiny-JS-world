f973af12607d1ae2f52281ed821ca2eac5656d61
Ruslan <ruslan.mozgovoii@gmail.com>
2020-12-13 16:29:28 +0200

    Light refactor - before ES6 Classes added

--- index.js
+++ index.js
@@ -8,74 +8,70 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-let Inhabitant = function (name, gender, saying, friendTo) {
+const Inhabitant = function (name, saying, friendTo, gender) {
   this.name = name;
   this.gender = gender;
   this.saying = saying;
   this.friendTo = friendTo;
 };
 
-let Human = function (name, gender, saying, friendTo) {
-  Inhabitant.call(this, name, gender, saying, friendTo);
+const Human = function (name, saying, friendTo, gender) {
+  Inhabitant.call(this, name, saying, friendTo, gender);
+  this.hands = 2;
+  this.legs = 2;
+  this.type = "human";
 };
 Human.prototype = Object.create(Inhabitant.prototype);
 Human.prototype.constructor = Human;
-Human.prototype.hands = "2 hands";
-Human.prototype.legs = "2 legs";
-Human.prototype.type = "human";
 
-let Man = function (name, saying, friendTo) {
-  Human.call(this, name, this.gender, saying, friendTo);
+const Man = function (name, saying, friendTo, gender = "male") {
+  Human.call(this, name, saying, friendTo, gender);
 };
 Man.prototype = Object.create(Human.prototype);
 Man.prototype.constructor = Man;
-Man.prototype.gender = "male";
 
-let Woman = function (name, saying, friendTo) {
-  Human.call(this, name, this.gender, saying, friendTo);
+const Woman = function (name, saying, friendTo, gender = "female") {
+  Human.call(this, name, saying, friendTo, gender);
 };
 Woman.prototype = Object.create(Human.prototype);
 Woman.prototype.constructor = Woman;
-Woman.prototype.gender = "female";
 
-let Pet = function (name, gender, saying, friendTo) {
-  Inhabitant.call(this, name, gender, saying, friendTo);
+const Pet = function (name, saying, friendTo, gender) {
+  Inhabitant.call(this, name, saying, friendTo, gender);
+  this.type = "pet";
 };
 Pet.prototype = Object.create(Inhabitant.prototype);
 Pet.prototype.constructor = Pet;
-Pet.prototype.type = "pet";
 
-let Dog = function (name, gender, friendTo) {
-  Pet.call(this, name, gender, this.saying, friendTo);
+const Dog = function (name, saying, friendTo, gender) {
+  Pet.call(this, name, saying, friendTo, gender);
+  this.legs = 4;
+  this.type = this.type + ": " + "dog";
 };
 Dog.prototype = Object.create(Pet.prototype);
 Dog.prototype.constructor = Dog;
-Dog.prototype.legs = "4 legs";
-Dog.prototype.tail = "1 tail";
-Dog.prototype.type = Dog.prototype.type + ": " + "dog";
-Dog.prototype.saying = "Bark";
 
-let Cat = function (name, gender, friendTo) {
-  Pet.call(this, name, gender, this.saying, friendTo);
+const Cat = function (name, saying, friendTo, gender) {
+  Pet.call(this, name, saying, friendTo, gender);
+  this.legs = 4;
+  this.type = this.type + ": " + "cat";
 };
 Cat.prototype = Object.create(Pet.prototype);
 Cat.prototype.constructor = Cat;
-Cat.prototype.legs = "4 legs";
-Cat.prototype.tail = "1 tail";
-Cat.prototype.type = Cat.prototype.type + ": " + "cat";
-Cat.prototype.saying = "Meow";
 
-let dog = new Dog("Ghost", "male", ["Monica", "Tom", "Everybody"]);
-let cat = new Cat("Tom", "male", ["Monica", "Chandler", "Everyone who feeds"]);
-let woman = new Woman("Monica", "Hello", ["Chandler", "Ghost"]);
-let man = new Man("Chandler", "Hi", ["Monica", "Tom"]);
+const dog = new Dog("Ghost", "Woof", ["Monica", "Tom", "Everybody"], "male");
+const cat = new Cat(
+  "Tom",
+  "Meow",
+  ["Monica", "Chandler", "Everyone who feeds"],
+  "male"
+);
+const woman = new Woman("Monica", "Hello", ["Chandler", "Ghost"]);
+const man = new Man("Chandler", "Hi", ["Monica", "Tom"]);
 
-let catWoman = new Cat("Cat-woman", "female", [
-  "Chandler dressed as a Pink Bunny",
-]);
-catWoman.legs = "2 legs";
-catWoman.hands = "2 hands";
-catWoman.tail = "1 cat-woman's tail";
+const catWoman = new Cat("Cat-woman", "female", ["Chandler"]);
+catWoman.legs = 2;
+catWoman.hands = 2;
 catWoman.type = "super-hero";
 
 // ======== OUTPUT ========
@@ -87,17 +83,15 @@ catWoman.type = "super-hero";
    so code reviewers might focus on a single file that is index.js.
    */
 
-let makeMessage = (obj) => {
-  let propsArr = [];
-  for (let propName in obj) {
-    propName !== "constructor"
-      ? propsArr.push(propName)
-      : console.log("constructor found");
-  }
-  return propsArr.map((propName) => obj[propName]).join("; ");
-};
+const makeMessage = (obj) =>
+  ["type", "name", "saying", "gender", "legs", "hands", "friendTo"]
+    .map((propName) =>
+      obj[propName] === undefined ? "none" : `${propName}: ${obj[propName]}`
+    )
+    .filter((prop) => prop !== "none")
+    .join(";\n");
 
-let world = [dog, cat, woman, man, catWoman];
+const world = [dog, cat, woman, man, catWoman];
 world.forEach((inhabitant) => print(makeMessage(inhabitant)));
 
 /* Print examples:
